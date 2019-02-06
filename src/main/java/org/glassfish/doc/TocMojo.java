18e514737a78f7afa2a37789be939394ef98037f
Bill Shannon <bill.shannon@oracle.com>
2019-02-06 14:44:57 -0800

    Change package to match Maven coordinates.

--- src/main/java/org/glassfish/doc/TocMojo.java
+++ src/main/java/org/glassfish/doc/TocMojo.java
@@ -14,7 +14,7 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
-package org.glassfish.toc;
+package org.glassfish.doc;
 
 import java.io.*;
 import java.util.*;
